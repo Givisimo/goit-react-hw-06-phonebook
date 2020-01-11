e0f501b712a0edd8fd16b609d045038638572f3c
Denys Plietnikov <givisimo@gmail.com>
2020-01-11 20:36:19 +0200

    Updates

---
+++ precache-manifest.d8e8b2954799513826c09f7d8909dc2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c30f671fa05b2d62319faebe1b369f6e",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "5e47340e0cf020f966eb",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.9867a545.chunk.css"
+  },
+  {
+    "revision": "6201ccb758325c6fdbcc",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.2dc0f85a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.2dc0f85a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e47340e0cf020f966eb",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.eafb394f.chunk.js"
+  },
+  {
+    "revision": "8ef94a824e6f3ee6d7e7",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b34c3905.js"
+  }
+]);
